922c45fc810240c315057ff1a9e0e68620e3bf07
ryusuke.miyaji <bluce826@gmail.com>
2020-01-17 19:37:02 +0900

    add:neccesary modle as file-saver tui-image-editor fabric feat:made imageEditor component

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ImageEditor />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ImageEditor from "./components/ImageEditor.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    ImageEditor
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/ImageEditor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="imageEditorApp">
+    <tui-image-editor ref="tuiImageEditor" :include-ui="useDefaultUI" :options="options"></tui-image-editor>
+  </div>
+</template>
+
+<script>
+import { ImageEditor } from "@toast-ui/vue-image-editor";
+// To use the basic UI, the svg files for the icons is required.
+import "tui-image-editor/dist/svg/icon-a.svg";
+import "tui-image-editor/dist/svg/icon-b.svg";
+import "tui-image-editor/dist/svg/icon-c.svg";
+import "tui-image-editor/dist/svg/icon-d.svg";
+
+// Load Style Code
+import "tui-image-editor/dist/tui-image-editor.css";
+import "file-saver";
+export default {
+  name: "ImageEditor",
+  components: {
+    "tui-image-editor": ImageEditor
+  },
+  props: {},
+  data() {
+    return {
+      useDefaultUI: true,
+      options: {
+        // for tui-image-editor component's "options" prop
+        cssMaxWidth: 700,
+        cssMaxHeight: 500
+      }
+    };
+  }
+};
+</script>
