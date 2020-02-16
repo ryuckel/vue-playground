81299cc7a3b380a8d321c908703037add69c5415
ryusuke.miyaji <bluce826@gmail.com>
2020-02-16 20:13:46 +0900

    remove image editor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <ImageEditor />
-  </div>
+	<div id="app"></div>
 </template>
 
 <script>
-import ImageEditor from "./components/ImageEditor.vue";
-
 export default {
-  name: "app",
-  components: {
-    ImageEditor
-  }
+	name: 'app',
+	components: {}
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  max-height: 100%;
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	max-height: 100%;
 }
 </style>
