d7a602c3ec91c214538423153566950393b1b6aa
ryusuke.miyaji <ryusuke.miyaji@persol.co.jp>
2020-03-02 13:06:48 +0900

    feat add heatmap.js and draw heatmap

---
+++ src/components/Heatmap.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper">
+   <h2>Demo</h2>
+    <div class="demo-wrapper">
+      <div class="heatmap">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+const h337 = require('heatmap.js')
+export default {
+  name: 'Heatmap',
+  props: {
+  },
+  data(){
+
+  },
+  methods:{
+    drawHeatmap(){
+var heatmapInstance = h337.create({
+  // only container is required, the rest will be defaults
+  container: document.querySelector('.heatmap')
+});
+
+// now generate some random data
+var points = [];
+var max = 0;
+var width = 840;
+var height = 400;
+var len = 200;
+
+while (len--) {
+  var val = Math.floor(Math.random()*100);
+  max = Math.max(max, val);
+  var point = {
+    x: Math.floor(Math.random()*width),
+    y: Math.floor(Math.random()*height),
+    value: val
+  };
+  points.push(point);
+}
+// heatmap data format
+var data = { 
+  max: max, 
+  data: points 
+};
+// if you have a set of datapoints always use setData instead of addData
+// for data initialization
+// heatmapInstance.setData(data);
+heatmapInstance.setData(data);
+    }
+  },
+  mounted(){
+    this.drawHeatmap()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.demo-wrapper { 
+    height:400px;
+    background:rgba(0,0,0,.03);
+    border:3px solid black;
+}
+.heatmap {
+    width:100%;
+    height:100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Buefy from 'buefy'
 import './assets/scss/app.scss'
-
+import heatmap from 'vue-heatmapjs'
+// import h337 from 'heatmapjs'
+// Vue.use(h337)
+Vue.use(heatmap)
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
